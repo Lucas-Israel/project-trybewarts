70992b2d2a60ac57d73cf7c18b757ea08feface2
Lucas Israel <lisraelalves92@gmail.com>
2022-06-07 18:18:37 -0300

    20 feito

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 const btnSub = document.querySelector('.submit');
 const btnAgree = document.querySelector('#agreement');
 const btnSub2 = document.querySelector('#submit-btn');
+const textArea = document.querySelector('#textarea');
+const caracteres = document.getElementById('counter');
+const form = document.querySelector('#evaluation-form');
 
 btnSub.addEventListener('click', () => {
   const email = document.querySelector('.email').value;
@@ -17,3 +20,18 @@ btnAgree.addEventListener('click', () => {
     btnSub2.disabled = false;
   }
 });
+
+const maxCarac = 500;
+
+textArea.addEventListener('keyup', () => {
+  const texts = textArea.value.length;
+  const counter = maxCarac - texts;
+  caracteres.textContent = counter;
+});
+
+// https://stackabuse.com/character-counter-for-text-areas-with-vanilla-javascript/
+
+// btnSub2.addEventListener('click', (event) => {
+//   event.preventDefault();
+//   form.style.display = 'none';
+// });
