71f55eadea691ff73dcce12439466d9a68983e02
Lucas Israel <lisraelalves92@gmail.com>
2022-06-07 17:00:44 -0300

    do 16 ao 19

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 const btnSub = document.querySelector('.submit');
+const btnAgree = document.querySelector('#agreement');
+const btnSub2 = document.querySelector('#submit-btn');
 
 btnSub.addEventListener('click', () => {
   const email = document.querySelector('.email').value;
@@ -7,5 +9,11 @@ btnSub.addEventListener('click', () => {
     alert('Olá, Tryber!');
   } else {
     alert('Email ou senha inválidos.');
+  }
+});
+
+btnAgree.addEventListener('click', () => {
+  if (btnAgree) {
+    btnSub2.disabled = false;
   }
 });
